0913a5f456d574dde0b3ac8fa00c1125fd9e5b77
boobo94 <militaru.bogdanalexandru@gmail.com>
2019-03-23 20:49:58 +0200

    Define interfaces for test data and personality type

---
+++ custom/intents/personality/interface.ts
@@ -0,0 +1,15 @@
+
+
+export interface TestStructure {
+    Statement: String
+    Type: PersonalityType
+}
+
+export enum PersonalityType {
+    R = "realistc",
+    I = "investigative",
+    A = "artistic",
+    S = "social",
+    E = "enterprising",
+    C = "conventional",
+}
