95407ca41d1ef7ccf3c4c10fcedea617c9ad4df9
boobo94 <militaru.bogdanalexandru@gmail.com>
2019-03-23 16:59:02 +0200

    Add localization

--- custom/interceptors/localization.ts
+++ custom/interceptors/localization.ts
@@ -1,7 +1,9 @@
 import { RequestInterceptor } from "ask-sdk-core";
-import * as i18n from "i18next";
 import * as sprintf from "i18next-sprintf-postprocessor";
-import { locales } from '../../locales/index';
+import { locales } from "../../locales";
+import { RequestAttributes } from "../lib/interfaces";
+import * as i18n from "i18next";
+
 
 type TranslationFunction = (...args: any[]) => string;
 
@@ -11,17 +13,16 @@ type TranslationFunction = (...args: any[]) => string;
 export const Localization: RequestInterceptor = {
     process(handlerInput) {
         const localizationClient = i18n.use(sprintf).init({
-            lng: handlerInput.requestEnvelope.request.locale,
+            lng: (handlerInput.requestEnvelope.request as any).locale,
             overloadTranslationOptionHandler: sprintf.overloadTranslationOptionHandler,
             resources: locales,
             returnObjects: true,
         });
 
-        const attributes = handlerInput.attributesManager.getRequestAttributes();
+        const attributes = handlerInput.attributesManager.getRequestAttributes() as RequestAttributes;
         attributes.t = function (...args: any[]) {
             return (localizationClient.t as TranslationFunction)(...args);
         };
-
         attributes.tr = function (key: any) {
             const result = localizationClient.t(key) as string[];
 
