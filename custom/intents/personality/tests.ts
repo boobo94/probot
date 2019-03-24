cac08112cbc52748ab05c50de5299b4b7593dc4c
boobo94 <militaru.bogdanalexandru@gmail.com>
2019-03-24 21:29:58 +0200

    Add all the statements for the first test

--- custom/intents/personality/tests.ts
+++ custom/intents/personality/tests.ts
@@ -46,6 +46,130 @@ const tests: Array<StatementStructure[]> = [
             Statement: "I like to do experiments",
             Type: PersonalityType.I
         },
+        {
+            Statement: "I like to teach or train people",
+            Type: PersonalityType.S
+        },
+        {
+            Statement: "I like trying to help people solve their problems",
+            Type: PersonalityType.S
+        },
+        {
+            Statement: "I like to take care of animals",
+            Type: PersonalityType.R
+        },
+        {
+            Statement: "I wouldn’t mind working 8 hours per day in an office",
+            Type: PersonalityType.C
+        },
+        {
+            Statement: "I like selling things",
+            Type: PersonalityType.E
+        },
+        {
+            Statement: "I enjoy creative writing",
+            Type: PersonalityType.A
+        },
+        {
+            Statement: "I enjoy science",
+            Type: PersonalityType.I
+        },
+        {
+            Statement: "I am quick to take on new responsibilities",
+            Type: PersonalityType.E
+        },
+        {
+            Statement: "I am interested in healing people",
+            Type: PersonalityType.S
+        },
+        {
+            Statement: "I enjoy trying to figure out how things work",
+            Type: PersonalityType.I
+        },
+        {
+            Statement: "I like putting things together or assembling things",
+            Type: PersonalityType.R
+        },
+        {
+            Statement: "I am a creative person",
+            Type: PersonalityType.A
+        },
+        {
+            Statement: "I pay attention to details",
+            Type: PersonalityType.C
+        },
+        {
+            Statement: "I like to do filing or typing",
+            Type: PersonalityType.C
+        },
+        {
+            Statement: "I like to analyze things (problems/situations)",
+            Type: PersonalityType.I
+        },
+        {
+            Statement: "I like to play instruments or sing",
+            Type: PersonalityType.A
+        },
+        {
+            Statement: "I enjoy learning about other cultures",
+            Type: PersonalityType.S
+        },
+        {
+            Statement: "I would like to start my own business",
+            Type: PersonalityType.E
+        },
+        {
+            Statement: "I like to cook",
+            Type: PersonalityType.R
+        },
+        {
+            Statement: "I like acting in plays",
+            Type: PersonalityType.A
+        },
+        {
+            Statement: "I am a practical person",
+            Type: PersonalityType.R
+        },
+        {
+            Statement: "I like working with numbers  or charts",
+            Type: PersonalityType.I
+        },
+        {
+            Statement: "I like to get into discussions about issues",
+            Type: PersonalityType.S
+        },
+        {
+            Statement: "I am good at keeping records of my work",
+            Type: PersonalityType.C
+        },
+        {
+            Statement: "I like to lead",
+            Type: PersonalityType.E
+        },
+        {
+            Statement: "I like working outdoors",
+            Type: PersonalityType.R
+        },
+        {
+            Statement: "I would like to work in an office",
+            Type: PersonalityType.C
+        },
+        {
+            Statement: "I’m good at math",
+            Type: PersonalityType.I
+        },
+        {
+            Statement: "I like helping people",
+            Type: PersonalityType.S
+        },
+        {
+            Statement: "I like to draw",
+            Type: PersonalityType.A
+        },
+        {
+            Statement: "I like to give speeches",
+            Type: PersonalityType.E
+        }
     ],
 ]
 export default tests
