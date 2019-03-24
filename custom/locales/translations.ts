b62acee991b91c9370f890247a6799549d43a24b
boobo94 <militaru.bogdanalexandru@gmail.com>
2019-03-24 17:37:17 +0200

    Add copies for the personality types

--- custom/locales/translations.ts
+++ custom/locales/translations.ts
@@ -14,6 +14,14 @@ export const locales = {
             "TEST_STARTED": "The test started. For the next questions you simply need to answer with 'Yes' or 'No'.",
             "TEST_ENDED": "The test ended. Your personality is %s",
 
+            // personalities
+            "REALISTIC_MSG": "These people are often good at mechanical or athletic jobs. Good college majors for Realistic people are: Agriculture, Health Assistant, Computers, Construction, Mechanic/Machinist, Engineering, Food and Hospitality.",
+            "INVESTIGATIVE_MSG": "These people like to watch, learn, analyze and solve problems. Good college majors for Investigative people are: Marine Biology, Engineering, Chemistry, Zoology, Medicine/Surgery, Consumer Economics, Psychology",
+            "ARTISTIC_MSG": "These people like to work in unstructured situations where they can use their creativity. Good majors for Artistic people are: Communications,Cosmetology,Fine and Performing Arts,Photography,Radio and TV,Interior Design,Architecture",
+            "SOCIAL_MSG": "These people like to work with other people, rather than things. Good college majors for  Social people are: Counseling, Nursing, Physical Therapy, Travel, Advertising, Public Relations, Education",
+            "ENTERPRISING_MSG": "These people like to work with others and enjoy persuading and and performing. Good college majors for Enterprising people are:,Fashion Merchandising,Real Estate,Marketing/Sales,Law,Political Science,International Trade ,Banking/Finance",
+            "CONVENTIONAL_MSG": "These people are very detail oriented,organized  and like to work with data. Good college majors  for Conventional people are: Accounting,Court Reporting,Insurance,Administration,Medical Records,Banking,Data Processing",
+
             // job search
             "SEARCH_JOBS_QUESTION": "Do you want to search for jobs?"
 
