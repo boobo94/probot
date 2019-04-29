bbb46a8a5bb4b5c803d64f606443aff17d0b6cdb
boobo94 <militaru.bogdanalexandru@gmail.com>
2019-04-29 21:31:27 +0300

    Add new error translation

--- custom/locales/translations.ts
+++ custom/locales/translations.ts
@@ -6,8 +6,6 @@ export const locales = {
             "GOODBYE_MSG": "Goodbye!",
             "HELLO_MSG": "If you want to start the test, simply ask `start the test`. In any moment yout can check your personality.",
             "HELP_MSG": "You can say hello to me!",
-            "ERROR_MSG": "Sorry, I can't understand the command. Please say again.",
-            "ERROR_UNEXPECTED_MSG": "Sorry, an unexpected error has occured. Please try again later.",
 
             // test intent
             "START_TEST_QUESTION": "Do you want to start the test?",
@@ -34,7 +32,13 @@ export const locales = {
             "CONVENTIONAL_MSG": "These people are very detail oriented,organized  and like to work with data. Good college majors  for Conventional people are: Accounting, Court Reporting, Insurance, Administration, Medical Records, Banking, Data Processing.",
 
             // job search
-            "SEARCH_JOBS_QUESTION": "Do you want to search for jobs?"
+            "SEARCH_JOBS_QUESTION": "Do you want to search for jobs?",
+
+            // errors
+            "ERROR_FINDING_JOBS": "Sorry but I couldn't connect to find a job, please try again.",
+            "ERROR_MSG": "Sorry, I can't understand the command. Please say again.",
+            "ERROR_UNEXPECTED_MSG": "Sorry, an unexpected error has occured. Please try again later.",
+
 
         }
     },
