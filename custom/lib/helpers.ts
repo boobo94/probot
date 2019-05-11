237b8792a78f86e17d557ba0d98a703b5509d261
boobo94 <militaru.bogdanalexandru@gmail.com>
2019-05-11 16:31:29 +0300

    Implement get slot values function

--- custom/lib/helpers.ts
+++ custom/lib/helpers.ts
@@ -1,5 +1,6 @@
 import { HandlerInput } from "ask-sdk";
 import { RequestAttributes, SessionAttributes, PersistentAttributes } from './interfaces';
+import { SlotValues, RequestTypes } from "./types";
 
 export function IsIntent(handlerInput: HandlerInput, ...intents: string[]): boolean {
     if (handlerInput.requestEnvelope.request.type === "IntentRequest") {
@@ -37,4 +38,94 @@ export function CreateError(message: string, name: string): Error {
     let error = new Error(message)
     error.name = name
     return error
+}
+
+export function GetSlotValues(handlerInput: HandlerInput): SlotValues {
+    if (handlerInput.requestEnvelope.request.type === RequestTypes.Intent) {
+        const filledSlots = handlerInput.requestEnvelope.request.intent.slots
+        const slotValues: SlotValues = {};
+
+        if (filledSlots) {
+            Object.keys(filledSlots).forEach((item) => {
+                const name = filledSlots[item].name;
+                const value = filledSlots[item].value;
+                const confirmationStatus = filledSlots[item].confirmationStatus;
+
+                if (filledSlots[item]
+                    && filledSlots[item].resolutions
+                    && filledSlots[item].resolutions!.resolutionsPerAuthority) {
+
+                    const resolutions = filledSlots[item].resolutions!.resolutionsPerAuthority!;
+
+                    let foundMatch = false;
+
+                    for (let resolution of resolutions) {
+                        switch (resolution.status.code) {
+                            case "ER_SUCCESS_MATCH":
+                                foundMatch = true;
+
+                                const valueWrappers = resolution.values;
+
+                                if (valueWrappers.length > 1) {
+                                    slotValues[name] = {
+                                        name: name,
+                                        value: value,
+                                        isMatch: true,
+                                        confirmationStatus: confirmationStatus,
+                                    };
+                                    break;
+                                }
+
+                                slotValues[name] = {
+                                    name: name,
+                                    value: value,
+                                    isMatch: true,
+                                    confirmationStatus: confirmationStatus,
+                                };
+                                break;
+                            case "ER_SUCCESS_NO_MATCH":
+                                if (!slotValues[name]) {
+                                    slotValues[name] = {
+                                        name: name,
+                                        value: value,
+                                        isMatch: false,
+                                        confirmationStatus: confirmationStatus,
+                                    };
+                                }
+                                break;
+                            default:
+                                break;
+                        }
+
+                        if (foundMatch) {
+                            break;
+                        }
+                    }
+                }
+                // slots which do not have any resolutions but have a value
+                // e.g. for AMAZON.NUMBER
+                else if (filledSlots[item]
+                    && !filledSlots[item].resolutions
+                    && filledSlots[item].value) {
+                    slotValues[name] = {
+                        name: name,
+                        value: value,
+                        isMatch: true,
+                        confirmationStatus: confirmationStatus,
+                    };
+                } else {
+                    slotValues[name] = {
+                        name: name,
+                        value: value,
+                        isMatch: false,
+                        confirmationStatus: confirmationStatus,
+                    };
+                }
+            });
+        }
+
+        return slotValues;
+    }
+    else
+        return {}
 }
