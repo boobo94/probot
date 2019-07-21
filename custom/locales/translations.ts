c6f3b819d8870ef8d0017619326ef6a5750fda83
boobo94 <militaru.bogdanalexandru@gmail.com>
2019-07-21 18:48:05 +0300

    Update translation messages

--- custom/locales/translations.ts
+++ custom/locales/translations.ts
@@ -2,7 +2,7 @@ export const locales = {
     'en-US': {
         translation: {
             "SKILL_NAME": "Probot",
-            "WELCOME_MSG": "Welcome to the Probot. You can start the test, check your personality results or find a job.",
+            "WELCOME_MSG": "Welcome to Probot. You can start a professional test, check your personality results or find a job. What are you interested for?",
             "GOODBYE_MSG": "Goodbye!",
             "HELLO_MSG": "If you want to start the test, simply ask `start the test`. In any moment yout can check your personality.",
             "HELP_MSG": "You can say hello to me!",
@@ -10,7 +10,7 @@ export const locales = {
             // test intent
             "START_TEST_QUESTION": "Do you want to start the test?",
             "TEST_STARTED": "The test started. For the next questions tell me if you agree or not.",
-            "TEST_ENDED": "The test ended. Your personality is %s. %s. Thank you for your time! You can start the test or check your results. What do you want next?",
+            "TEST_ENDED": "The test ended. Your personality is %s. %s Thank you for your time! You can start the test or check your results. What do you want next?",
 
             // check my personality
             "CHECK_PERSONALITY": "Your personality is %s. %s",
@@ -28,7 +28,7 @@ export const locales = {
             "INVESTIGATIVE_MSG": "<break strength=\"strong\"/> Investigative people like to watch, learn, analyze and solve problems. Good college majors for Investigative people are: Marine Biology, Engineering, Chemistry, Zoology, Medicine or Surgery, Consumer Economics, Psychology. ",
             "ARTISTIC_MSG": "<break strength=\"strong\"/> Artistic people like to work in unstructured situations where they can use their creativity. Good majors for Artistic people are: Communications, Cosmetology, Fine and Performing Arts, Photography, Radio and TV, Interior Design, Architecture. ",
             "SOCIAL_MSG": "<break strength=\"strong\"/> Social people like to work with other people rather than things. Good college majors for  Social people are: Counseling, Nursing, Physical Therapy, Travel, Advertising, Public Relations, Education. ",
-            "ENTERPRISING_MSG": "<break strength=\"strong\"/> Enterprising people like to work with others and enjoy persuading and and performing. Good college majors for Enterprising people are: Fashion Merchandising, Real Estate, Marketing or Sales, Law, Political Science, International Trade, Banking or Finance. ",
+            "ENTERPRISING_MSG": "<break strength=\"strong\"/> Enterprising people like to work with others and enjoy persuading and performing. Good college majors for Enterprising people are: Fashion Merchandising, Real Estate, Marketing or Sales, Law, Political Science, International Trade, Banking or Finance. ",
             "CONVENTIONAL_MSG": "<break strength=\"strong\"/> Conventional people are very detail oriented,organized  and like to work with data. Good college majors for Conventional people are: Accounting, Court Reporting, Insurance, Administration, Medical Records, Banking, Data Processing. ",
 
             // job search
@@ -41,10 +41,10 @@ export const locales = {
             "URL": "URL",
 
             // errors
-            "ERROR_FINDING_JOBS": "Sorry but I couldn't connect to find a job, please try again.",
-            "ERROR_NO_MORE_JOBS": "Sorry but I don't have any new job. You can search again.",
-            "ERROR_MSG": "Sorry, I can't understand the command. Please say again.",
-            "ERROR_UNEXPECTED_MSG": "Sorry, an unexpected error has occured. Please try again later.",
+            "ERROR_FINDING_JOBS": "Sorry but I cannot find that job, please try again.",
+            "ERROR_NO_MORE_JOBS": "Sorry but I don't have any jobs. Please search with a different query.",
+            "ERROR_MSG": "Huston, we have a problem. Please repeat!",
+            "ERROR_UNEXPECTED_MSG": "I lost a transistor, so I don't know this one. How can I help you?",
 
         }
     },
